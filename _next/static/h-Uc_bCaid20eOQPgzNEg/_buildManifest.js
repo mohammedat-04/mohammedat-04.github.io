4c6dfb0f066e9542f0359b95baeb226464c6b19a
Mohamed Helali <mohamed.helali@stud.uni-hannover.de>
2026-04-11 21:14:40 +0200

    Updates

---
+++ _next/static/h-Uc_bCaid20eOQPgzNEg/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Mohamed-Atef-Helali//_next/:path+",
+        "destination": "/Mohamed-Atef-Helali/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
